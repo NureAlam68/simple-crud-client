aab1a66051b681a0f478731c3ae38124480004da
NureAlam68 <nurealam151068@gmail.com>
2024-11-26 16:30:41 +0600

    creat user

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  const handleAddUser = e => {
+  const handleAddUser = (e) => {
     e.preventDefault();
     const form = e.target;
     const name = form.name.value;
     const email = form.email.value;
-    const user = {name, email}
-    console.log(user)
-  }
+    const user = { name, email };
+    console.log(user);
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if(data.insertedId) {
+        alert('User added successfully')
+        form.reset()
+      }
+    })
+    
+  };
 
   return (
     <>
@@ -22,9 +36,8 @@ function App() {
         <br />
         <input type="submit" value="Add User" />
       </form>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
