0eef6ce9f0ce0b10e2f15a9d05094ac70ca9bf70
NureAlam68 <nurealam151068@gmail.com>
2024-11-26 21:02:58 +0600

    read and delete user

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
 
-    const users = useLoaderData()
+    const loadedUser = useLoaderData()
+
+    const [users, setUsers] = useState(loadedUser)
+
+    const handleDelete = id => {
+        console.log(id)
+        fetch(`http://localhost:5000/users/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.deletedCount > 0) {
+                alert('deleted successfully');
+                const remainingUser = users.filter(user => user._id !== id);
+                setUsers(remainingUser)
+            }
+        })
+    }
 
     return (
         <div>
             {users.length}
             <div>
                 {
-                    users.map(user => <p key={user._id}>{user.name}: {user.email}</p>)
+                    users.map(user => <p key={user._id}>{user.name}: {user.email} 
+                    <button
+                     onClick={() => handleDelete(user._id)}
+                    >X</button></p>)
                 }
             </div>
         </div>
